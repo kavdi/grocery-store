64d9594630d2bbd454a2fd168b83bd2e3f8e9a74
kavdihodgson <kavdyjh@gmail.com>
2017-09-21 09:41:15 -0700

    started server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const EXPRESS = require('express');
+const PG = require('pg');
+const BPS = require('body-parser');
+const APP = EXPRESS();
+const PORT = process.env.PORT || 3000;
+
+APP.use(EXPRESS.static('public'));
